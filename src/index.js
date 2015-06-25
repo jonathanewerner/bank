53fe60831fc3f18a9db02d9b76b41c60934f1d38
Jonathan Werner <jwerner@posteo.de>
2015-06-25 13:26:07 +0200

    more feeeatures.

--- src/index.js
+++ src/index.js
@@ -2,72 +2,238 @@ import figo from 'figo';
 import fs from 'fs';
 import moment from 'moment';
 import app from './server.js';
+import _ from 'lodash';
+import open from 'open';
+import accounting from 'accounting';
+import Table from 'cli-table';
 
-const args = process.argv.slice(2);
-
-import {SESSION_TOKEN} from '../configuration/constants.js';
+import {REFRESH_TOKEN, CLIENT_ID, CLIENT_SECRET, CALLBACK_URL} from '../configuration/constants.js'
 const DATABASE_FILE = 'database/transactions.json';
-try {
-  var transactions = JSON.parse(fs.readFileSync(DATABASE_FILE, 'utf8'));
-} catch (e) {
-  var transactions = [];
+const ALIASES_FILE = 'database/aliases.json';
+const CATEGORIES_FILE = 'database/categories.json';
+const TIME_TAKEN = 'Time taken';
+const RUN_ONLINE = false;
+
+let SUM = 0;
+
+const currencyFormat = {
+  thousandSep: ',',
+  decimalSep: '.',
+  precision: 2,
+  format: '%v%s',
+  symbol: '€'
 }
 
+function formatEuro(euroAmount) {
+  const {symbol, precision, thousandSep, decimalSep, format} = currencyFormat;
+  return accounting.formatMoney(
+      euroAmount, symbol, precision, thousandSep, decimalSep, format);
+}
 
-if (args[0] === 'get-token') {
-  console.info('1) Open localhost:3000/page \n2) copy your token into configuration/constants.js -> SESSION_TOKEN\n\n');
-  const server = app.listen(3000, function() {
-    console.log('started server on port ' + server.address().port);
+function r(regexString) {
+  return new RegExp(regexString, 'i');
+}
+
+const aliases = readJson(ALIASES_FILE);
+
+function readJson(f) {
+ return JSON.parse(fs.readFileSync(f), 'utf-8');
+}
+
+function startup() {
+  const args = process.argv.slice(2);
+  if (args[0] === 'token') {
+    open('http://localhost:3000/logout', 'google-chrome-beta');
+    open('http://localhost:3000/page', 'google-chrome-beta');
+
+    const server = app.listen(3000, function() {
+      console.log('started server on port ' + server.address().port);
+    });
+
+  } else {
+      try {
+        if (RUN_ONLINE) {
+          getSessionToken(main);
+        } else {
+          main();
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+}
+
+function getSessionToken(cb) {
+  const connection = new figo.Connection(CLIENT_ID, CLIENT_SECRET, CALLBACK_URL);
+
+  console.info('Obtaining access token with REFRESH_TOKEN');
+  console.time(TIME_TAKEN);
+  connection.obtain_access_token(REFRESH_TOKEN, null, (err, result) => {
+    console.info('Got an access token');
+    console.timeEnd(TIME_TAKEN);
+    if (result) {
+      cb(result.access_token);
+    } else {
+      console.log(err);
+    }
   });
 }
 
-if (!SESSION_TOKEN) {
-  console.error('You need to generate a session token by calling this thing with like `this-thing.js get-token` and following the instructions.');
-  process.exit(1);
+function fetchNewTransactions(transactions, sessionToken) {
+  const session = new figo.Session(sessionToken);
+  const lastTransactionID = transactions.length ?
+    transactions[0].transaction_id : undefined;
+
+  console.info('Getting transactions...');
+  console.time(TIME_TAKEN);
+
+  const options = lastTransactionID ? {since: lastTransactionID} : {};
+
+  return new Promise((resolve, reject) => {
+    session.get_transactions(options, (err, newTransactions) => {
+      if (err) {
+        reject(err);
+      } else {
+
+        console.info(`Got ${newTransactions.length} new transactions.`);
+        console.timeEnd(TIME_TAKEN);
+        transactions = [...newTransactions, ...transactions];
+        fs.writeFileSync(DATABASE_FILE, JSON.stringify(transactions, null, '\t'));
+        resolve(transactions);
+      }
+    });
+  });
 }
 
+async function main(sessionToken) {
+  try { /// reaaaally ? 
+  let transactions = fs.existsSync(DATABASE_FILE) ?
+    JSON.parse(fs.readFileSync(DATABASE_FILE, 'utf8')) : [];
 
-const session = new figo.Session(SESSION_TOKEN);
-const lastTransactionID = transactions.length ?
-  transactions[0].transaction_id : undefined;
-
-console.info('Getting transactions...');
-console.time('Time taken');
-
-const options = lastTransactionID ? {since: lastTransactionID} : {};
-session.get_transactions(options, (err, newTransactions) => {
-  if (err) {
-    console.info('[index.js] ', err);
-  } else {
-
-    console.info(`Got ${newTransactions.length} new transactions.`);
-    console.timeEnd('Time taken');
-    transactions = [...newTransactions, ...transactions];
-    fs.writeFileSync(DATABASE_FILE, JSON.stringify(transactions, null, '\t'));
-    const fromThisMonth = transactionsFromThisMonth(transactions);
-    // console.info('[index.js] ', fromThisMonth);
-    fromThisMonth.forEach((transaction) => {
-      const {booking_date, name, purpose, amount, currency} = transaction;
-      const dateRelative = moment(booking_date).fromNow();
-      console.info(`${dateRelative}: ${amount}EU from ${name.split(' ').slice(0,2)}`);
-    })
-
+  if (sessionToken) {
+    transactions = await fetchNewTransactions(transactions, sessionToken);
   }
-});
+  const ALL = false;
+
+  const categories = readJson(CATEGORIES_FILE);
+  const fromThisMonth = transactionsFromThisMonth(transactions);
+  const recurringTransactionsFromThisMonth =
+    filterWithRegex(fromThisMonth, categories.recurring);
+
+  if (ALL) {
+    printTable('All Transactions', fromThisMonth.reverse());
+    return;
+  }
+
+  // [1] print recurring transactions
+  printTable('Recurring Transactions', recurringTransactionsFromThisMonth.reverse());
+
+  console.info('');
+
+  // [2] print summaries
+  categories.summarized.forEach(summaryItem => {
+    const transactionsForSummaryItem =
+      filterWithRegex(fromThisMonth, summaryItem.regex);
+
+    printSummary(summaryItem.title, transactionsForSummaryItem)
+
+  });
+
+
+  // [3] print rest
+  // r(string) creates a case-insensitive regex from the given string
+  const restTransactions = fromThisMonth.filter(transaction => {
+    return !transaction.name.match(r(categories.recurring)) && 
+      !_.any(categories.summarized, summaryItem => 
+          transaction.name.match(r(summaryItem.regex)));
+  });
+
+  printTable('Rest', restTransactions);
+
+  console.info('OVERALL SUM:', formatEuro(SUM));
+
+  } catch (err) { // this really sucks. does somebody know a better way
+                  // to deal with error handling in es7 async functions..
+    console.info('[index.js] ', err.stack);
+  }
+}
+
+function filterWithRegex(transactions, regexString, invert=false) {
+  const regex = new RegExp(regexString, 'i');
+  return transactions.filter(transaction => {
+    const result = transaction.name.match(regex);
+    return invert ? !result : result;
+  });
+}
+
+function printSummary(title, transactions) {
+  // console.info('');
+  const sum = _.sum(transactions.map(t => t.amount));
+  const sumFormatted = formatEuro(sum);
+  console.info(`${title.toUpperCase()}: ${sumFormatted}`);
+}
+
+function printTable(title, transactions, showSum=true) {
+
+  function alias(fullName) {
+    const aliasKey = _.find(Object.keys(aliases), namePart =>
+        fullName.match(new RegExp(namePart)))
+      return aliases[aliasKey];
+  }
+
+  const amounts = transactions.map(t => t.amount);
+  const sumOfTable = _.sum(amounts);
+  SUM += sumOfTable;
+  if (showSum) amounts.push(sumOfTable);
+
+  const {symbol, precision, thousandSep, decimalSep, format} = currencyFormat;
+
+  const amountsFormatted =
+    accounting.formatColumn(
+      amounts, symbol, precision, thousandSep, decimalSep, format);
+
+  const table = new Table({
+    chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''},
+    head: ['Date', 'Name', 'Amount'],
+    colWidths: [7, 24, 12]
+  });
+
+  console.info(`\n${title.toUpperCase()}`);
+
+  const sumRow = {name: 'SUM', booking_date: undefined};
+  const rows = showSum ? [...transactions, sumRow] : transactions;
+  rows.forEach((t, idx) => {
+    const date = t.booking_date ?
+      moment(t.booking_date).format('DD.MM') : '';
+    // const description = alias(t.name) || t.name + ` (${t.purpose})`;
+    const description = alias(t.name) || t.name;
+    // const description = t.name + ` (${t.purpose})`;
+    table.push([date, description, amountsFormatted[idx]]);
+  });
+  console.info(table.toString());
+}
+
+function printTransaction(transaction) {
+  const VERBOSE = false;
+  const {booking_date, name, purpose, amount, currency} = transaction;
+  const date = moment(booking_date).format('DD.MM');
+  console.info(VERBOSE ?
+    `${date}: ${amount}€\nNAME:\t\t${name}\nPURPOSE:\t${purpose}\n` :
+    `${date}: ${amount}€ - ${name}`);
+}
+
 
 function transactionsFromThisMonth(transactions) {
+  const monthOffset = 1;
+  const endOfMonth = moment().subtract(monthOffset, 'month').endOf('month');
+  const startOfMonth = moment().subtract(monthOffset, 'month').startOf('month');
   return transactions.filter((transaction) => {
     const date = moment(transaction.booking_date);
-    const now = moment();
-    const startOfMonth = moment().startOf('month');
-    return date.isBetween(startOfMonth, now, 'day');
+    const foo = date.isBetween(startOfMonth, endOfMonth, 'minute');
+    return foo;
+    // console.info('[index.js] ', foo, date.toString());
   });
-  
+
 }
 
-// async function getUser() {
-//   return await session.get_user();
-// }
-
-
-
+startup();
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@
 import figo from 'figo';
 import express from 'express';
 const router = express.Router(); // eslint-disable new-cap
-import {CLIENT_ID, CLIENT_SECRET, CALLBACK_URL} from '../configuration/constants.js';
+import {CLIENT_ID, CLIENT_SECRET, CALLBACK_URL} from '../configuration/constants.js'
 
 const PORT = 3000;
 
@@ -10,78 +10,22 @@ const connection = new figo.Connection(CLIENT_ID, CLIENT_SECRET, CALLBACK_URL);
 
 const DUMMY_SAFEGUARD = 'qweqwe';
 
-
-// router.param('account_id', function(req, res, next, id) {
-//   req.account_id = id;
-//   next();
-// });
-
-
 router.get('/logout', function(req, res) {
   req.session = null;
   res.redirect('/page');
 });
 
-
-// router.get('/:account_id', function(req, res, next) {
-//   if (!req.session.figo_token) {
-//     res.redirect(connection.login_url(DUMMY_SAFEGUARD, 'accounts=ro transactions=ro balance=ro user=ro'));
-//   } else {
-//     const session = new figo.Session(req.session.figo_token);
-//     // session.get_user((err, user) => {
-//     //   session.get_accounts((err, accounts) => {
-//     //     session.get_account(req.account_id, (err, account) => {
-//     //       account.get_transactions(null, (err, transactions) =>{
-//     //         console.info('[routes.js] ', transactions);
-//     //         res.end();
-//     //         // res.render('index', { accounts: accounts, user: user, transactions: transactions, current_account: account });
-//     //       });
-//     //     });
-//     //   });
-//     // });
-//   }
-// });
-
-
-// router.get('/callback', (req, res) => {
-//   console.info('[routes.js callback] ', req.query.code);
-//   connection.obtain_access_token(req.query.code, null, (err, result) => {
-//     if (result) {
-//       req.session.figo_token = result.access_token;
-//     } else {
-//       console.log(err);
-//     }
-//     res.redirect('/page');
-//   })
-
-
 router.get('/page', (req, res) => {
   // if user isn't logged in, start oauth flow
   if (!req.session.figo_token) {
     console.info('[routes.js] ', 'we don\'t have a figo token; starting login process');
-    res.redirect(connection.login_url(DUMMY_SAFEGUARD, 'accounts=ro transactions=ro balance=ro user=ro'));
+    res.redirect(connection.login_url(DUMMY_SAFEGUARD, 'accounts=ro transactions=ro balance=ro user=ro offline'));
   } else {
     console.info('[routes.js] ', 'we have a figo session going');
     const session = new figo.Session(req.session.figo_token);
     res.write(req.session.figo_token);
     res.end();
-    // session.get_user((err, user) => {
-    //   if (err) {
-    //     next(err);
-    //   }
-    //   res.write(user); res.end();
-    // });
   }
-
-      // session.get_accounts(function(err, accounts) {
-      //   session.get_transactions(null, function(err, transactions){
-      //     res.write('foo');
-      //     res.end();
-          // res.render('index', { accounts: accounts, user: user, transactions: transactions });
-        // });
-      // });
-    // });
-  // }
 });
 
 // callback url : root
@@ -93,6 +37,7 @@ router.get('/', (req, res) => {
 
   console.info('[routes.js] ', 'obtaining access token ...');
   connection.obtain_access_token(req.query.code, null, (err, result) => {
+    console.info('[routes.js result:] ', result);
     if (result) {
       req.session.figo_token = result.access_token;
     } else {
@@ -104,37 +49,6 @@ router.get('/', (req, res) => {
 
 export default router;
 
-// function startLogin() {
-//   // Open web browser to kick of the login process.
-//   open(connection.login_url("qweqwe"));
-// }
-
-// function processRedirect(authorization_code, state) {
-//   // Handle the redirect URL invocation from the initial startLogin call.
-
-//   // Ignore bogus redirects.
-//   if (state !== "qweqwe") {
-//     return;
-//   }
-
-//   // Trade in authorization code for access token.
-//   const token = connection.obtain_access_token(authorization_code, null, function(error, token) {
-//     if (!error) {
-
-//       // Start session.
-//       var session = new figo.Session(token.access_token);
-
-//       // Print out list of account numbers.
-//       session.get_accounts(function(error, accounts) {
-//         if (!error) {
-//           accounts.forEach(function(account) {
-//             console.log(account.account_number);
-//           })
-//         }
-//       });
-//     }
-//   });
-// };
 
 /**
   OAUTH Terms:
